0a07db72a0454c5a4f314e67ab1b32f9fa9e9142
garlicsauce <adrian.michalik@wp.pl>
2019-07-23 22:49:20 +0200

    Initial commit

---
+++ src/main/java/io/garlicsauce/dateplanner/DatePlannerApplication.java
@@ -0,0 +1,13 @@
+package io.garlicsauce.dateplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatePlannerApplication.class, args);
+	}
+
+}
